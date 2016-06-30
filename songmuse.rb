c2559bb409b494629b1cbdf40039a6afbcfc9e05
Brian Kelly <brian@fetlife.com>
2016-06-30 17:17:45 -0400

    Adds scale and better layout

--- songmuse.rb
+++ songmuse.rb
@@ -3,10 +3,15 @@ require 'sinatra/reloader' if development?
 
 get '/' do
   @key = %w(C D E F G A B).sample
-  @genre = %w(alternative blues classical country dance electronic hiphop industrial jazz metal pop R&B rock).sample
+  @scale = %w(Major Dorian Phrygian Lydian Mixolydian Minor Locrian).sample
+  @genre = %w(Alternative Blues Classical Country Dance Electronic Hiphop Industrial Jazz Metal Pop R&B Rock).sample
   @time_signature = ["4/4", "6/4", "6/8"].sample
   @tempo = (60..150).to_a.sample
   @arrangement = "Verse Chorus Verse Chorus Bridge Verse Chorus"
-  @verse_chords = ["I vi IV V", "I vi ii V", "I ii IV V"].sample
+
+  chords = ["I vi IV V", "I vi ii V", "I ii IV V"]
+  @verse_chords = chords.sample
+  @chorus_chords = chords.sample
+
   slim :index
 end
